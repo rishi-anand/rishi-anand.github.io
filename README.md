4ae15ca82729eaf6dd6ba27ee3688c860c1a2112
Rishi <risanand@cisco.com>
2018-06-12 00:25:35 +0530

    editing readme files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# rishi-anand.github.io
+# Rishi Anand
+
+****Web Resume**** : ****https://rishi-anand.github.io/****
+
+![](resources/1.png?raw=true "Title")
+![](resources/2.png?raw=true "Title")
+![](resources/3.png?raw=true "Title") 
+![](resources/4.png?raw=true "Title") 
+![](resources/5.png?raw=true "Title") 
+![](resources/6.png?raw=true "Title") 
+![](resources/7.png?raw=true "Title") 
+![](resources/8.png?raw=true "Title") 
+![](resources/9.png?raw=true "Title") 
+![](resources/10.png?raw=true "Title")
--- scripts/expand.js
+++ scripts/expand.js
@@ -1,13 +1,15 @@
 $("#show_more").show();
- $("#text").hide();
- $("#less").hide();
- $("#more").click( function() {
-   $("#text").show();
-   $("#less").show();
-   $("#more").hide();
- });
- $("#less").click( function() {
-   $("#text").hide();
-   $("#less").hide();
-   $("#more").show();
- }); 
+$("#text").hide();
+$("#less").hide();
+
+$("#more").click( function() {
+	$("#text").show();
+	$("#less").show();
+	$("#more").hide();
+});
+
+$("#less").click( function() {
+	$("#text").hide();
+	$("#less").hide();
+	$("#more").show();
+});
