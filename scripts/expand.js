0b2001e28e9720a3a49763122a2d2868e0a40651
Rishi <risanand@cisco.com>
2018-06-11 22:44:08 +0530

    editing work and adding education

--- scripts/expand.js
+++ scripts/expand.js
@@ -1,18 +1,3 @@
-// $("#short_desc").show();
-// $("#show_more").show();
-// $("#more_desc").hide();
-// $("#show_less").hide();
-// $("#show_more").click( function() {
-// 	$("#more_desc").show();
-// 	$("#show_less").show();
-// 	$("#show_more").hide();
-// });
-// $("#show_less").click( function() {
-// 	$("#show_more").show();
-// 	$("#more_desc").hide();
-// 	$("#show_less").hide();
-// });
-
 $("#show_more").show();
  $("#text").hide();
  $("#less").hide();
@@ -25,4 +10,4 @@ $("#show_more").show();
    $("#text").hide();
    $("#less").hide();
    $("#more").show();
- });
+ }); 
