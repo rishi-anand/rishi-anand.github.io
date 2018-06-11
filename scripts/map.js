9d25e1287916c46602f5bdd4ddca22932158ef00
Rishi <risanand@cisco.com>
2018-06-11 20:54:17 +0530

    map init

--- scripts/map.js
+++ scripts/map.js
@@ -1,7 +1,9 @@
-function myMap() {
-var mapOptions = {
-    center: new google.maps.LatLng(12.918181700000002, 77.67589420000002),
-    zoom: 13
-}
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
 }
